6289c2192ec3e00c5127c42428e1db06bb86a005
xKawaiiMugi <54444757+xKawaiiMugi@users.noreply.github.com>
2023-05-21 16:04:08 +0200

    Add files via upload

---
+++ awoo-bot/commands/awoo.js
@@ -0,0 +1,25 @@
+// made by https://twitter.com/xkawaimugi via chatgpt c:
+
+const { SlashCommandBuilder } = require('discord.js');
+const fs = require('fs');
+const path = require('path');
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName('awoo')
+    .setDescription('awooooooooooooo.'),
+  async execute(interaction) {
+    const awooDirectory = './awoo'; // Directory path where the awoo images are located
+
+    try {
+      const files = fs.readdirSync(awooDirectory);
+      const randomIndex = Math.floor(Math.random() * files.length);
+      const randomFile = files[randomIndex];
+      const attachmentUrl = path.join(awooDirectory, randomFile);
+      await interaction.reply({ content: 'Awoo!', files: [attachmentUrl] });
+    } catch (error) {
+      console.error(error);
+      await interaction.reply({ content: 'Failed to execute the command.', ephemeral: true });
+    }
+  },
+};
