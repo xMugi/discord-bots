a2ec2995321db864f8fbd12776c042a006d4b968
xKawaiiMugi <54444757+xKawaiiMugi@users.noreply.github.com>
2023-05-25 01:44:00 +0200

    Update awoo.js

--- awoo-bot/commands/awoo.js
+++ awoo-bot/commands/awoo.js
@@ -1,22 +1,25 @@
-// made by https://twitter.com/xkawaimugi via chatgpt c:
-
 const { SlashCommandBuilder } = require('discord.js');
 const fs = require('fs');
 const path = require('path');
+
+const awooUrlsFile = path.join(__dirname, '..', 'config', 'awooUrls.json');
 
 module.exports = {
   data: new SlashCommandBuilder()
     .setName('awoo')
     .setDescription('awooooooooooooo.'),
   async execute(interaction) {
-    const awooDirectory = './awoo'; // Directory path where the awoo images are located
-
     try {
-      const files = fs.readdirSync(awooDirectory);
-      const randomIndex = Math.floor(Math.random() * files.length);
-      const randomFile = files[randomIndex];
-      const attachmentUrl = path.join(awooDirectory, randomFile);
-      await interaction.reply({ content: 'Awoo!', files: [attachmentUrl] });
+      // Read existing fileUrls from awooUrls.json
+      const fileUrlsData = fs.readFileSync(awooUrlsFile, 'utf-8');
+      const fileUrls = JSON.parse(fileUrlsData);
+
+      // Select a random URL from the fileUrls
+      const randomIndex = Math.floor(Math.random() * fileUrls.length);
+      const randomUrl = fileUrls[randomIndex];
+
+      // Post the response with the embedded image
+      await interaction.reply({ content: 'Awoo!', embeds: [{ image: { url: randomUrl } }] });
     } catch (error) {
       console.error(error);
       await interaction.reply({ content: 'Failed to execute the command.', ephemeral: true });
